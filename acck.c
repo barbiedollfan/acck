ffc80146d7c41f9c62db6925af377c1926f01200
barbiedollfan <timojhildebrand@gmail.com>
2025-09-04 22:10:03 -0400

    Refactored tokenizing logic

--- acck.c
+++ acck.c
@@ -5,55 +5,60 @@
 
 #define TRUE 1
 
+struct Token {
+    char* tokenName;
+    char* value;
+};
+
+struct Tokens {
+    int tokenCount;
+    int capacity;
+    char** tokenStrings;
+};
+
+void appendToken(struct Tokens* tokens, char* token) {
+    if (tokens->tokenCount >= tokens->capacity) {
+                tokens->capacity *= 2;
+                char** tempTokenStrings = realloc(tokens->tokenStrings, tokens->capacity * sizeof(char*));
+                tokens->tokenStrings = tempTokenStrings;
+    }
+    tokens->tokenStrings[tokens->tokenCount] = malloc(strlen(token) * sizeof(char) + 1);
+    strcpy(tokens->tokenStrings[tokens->tokenCount], token);
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         fprintf(stderr, "No input file given.\n");
         return 1;
     }
-
+    struct Tokens tokens = {0, 1, NULL};
     FILE* fptr;
     fptr = fopen(argv[1], "r");
     if (fptr == NULL) {
         fprintf(stderr, "File or directory '%s' not found.\n", argv[1]);
         return 1;
     }
-    int tokenCount = 0;
-    int capacity = 1;
-    char** tokens = (char**)malloc(capacity * sizeof(char*));
+    tokens.tokenStrings = (char**)malloc(tokens.capacity * sizeof(char*));
     char tmp[64] = "";
     while (TRUE) {
         char current = fgetc(fptr);
         if (isspace(current)) continue;
         else if (isalpha(current)) {
-            char str[2] = "";
-            str[0] = current;
-            str[1] = '\0';
-            strcat(tmp, str);
-            
-            char next = fgetc(fptr);
-            for (; isalnum(next); current = next, next = fgetc(fptr)) {
-                char str[2] = "";
-                str[0] = next;
-                str[1] = '\0';
-                strcat(tmp, str);
+            tmp[strlen(tmp)] = current;     // Append character to token buffer
+            for (current = fgetc(fptr); isalnum(current); current = fgetc(fptr)) {     // Peek at next character until it is not a valid part of a keyword or identifier
+                tmp[strlen(tmp)] = current;
             }
-            ungetc(next, fptr);
-            if (tokenCount >= capacity) {
-                capacity *= 2;
-                char** tempTokens = realloc(tokens, capacity * sizeof(char*));
-                tokens = tempTokens;
-            }
-            tokens[tokenCount] = malloc(strlen(tmp) * sizeof(char) + 1);
-            strcpy(tokens[tokenCount], tmp);
-            strcpy(tmp, "");
-            tokenCount++;
+            ungetc(current, fptr);     // Put back last consumed character into input stream
+            appendToken(&tokens, tmp); 
+            memset(tmp, 0, strlen(tmp));    // Clear token buffer for next iteration
+            tokens.tokenCount++;
             continue;
         }
         break;
     }
-    for (int i = 0; i < tokenCount; i++) {
-        free(tokens[i]);
+    for (int i = 0; i < tokens.tokenCount; i++) {
+        free(tokens.tokenStrings[i]);
     }
-    free(tokens);
+    free(tokens.tokenStrings);
     return 0;
 }
